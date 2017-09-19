c9d66869c79ac1fd00ae59bdbed5c79d3ab19fd4
RyanKolbe <31966635+RyanKolbe@users.noreply.github.com>
2017-09-19 20:49:52 +0200

    Smooth Mover Methods added
    Smooth Mover Methods included.

--- Airplane.java
+++ Airplane.java
@@ -13,7 +13,56 @@ public class Airplane extends Actor
 
     }
 
+    private double preciseX;
+    private double preciseY;
     public int health = 100;
+
+    /**
+     * Move forward by specified distance and overrides the method in Actor.
+     */
+    @Override
+    public void move(int distance)
+    {
+        move((double)distance);
+    }
+
+    /**
+     * Moved forward by the specified precise distance.
+     */
+    public void move(double distance)
+    {
+        double radians = Math.toRadians(getRotation());
+        double dx = Math.cos(radians) * distance;
+        double dy = Math.sin(radians) * distance;
+        setLocation(preciseX + dx, preciseY + dy);
+    }
+
+    /**
+     * Set the location using precise coordinates.
+     */
+    public void setLocation(double x, double y)
+    {
+        preciseX = x;
+        preciseY = y;
+        super.setLocation((int) (x + 0.5), (int) (y + 0.5));
+    }
+
+    /**
+     * Returns the precise x-coordinate as a double.
+     */
+    public double getPreciseX()
+    {
+        return preciseX;
+    }
+
+    /**
+     * Returns the precise y-coordinate as a double.
+     */
+
+    public double getPreciseY()
+    {
+        return preciseY;
+    }
 
     /**
      * Act - do whatever the Airplane wants to do. This method is called whenever
@@ -29,6 +78,4 @@ public class Airplane extends Actor
         Projectile bullet = new Projectile(inRotation);
         getWorld().addObject(bullet, getX(), getY());
     }
-    
-    
 }
